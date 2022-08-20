e0f9ffdacac234f51401111e418e1441283e3e87
alphacpc <alphacpc@gmail.com>
2022-08-20 16:15:55 +0000

    Updated tools container

--- src/components/Tools.js
+++ src/components/Tools.js
@@ -2,29 +2,16 @@ import React from 'react'
 
 import "./../assets/css/Tools.css"
 
-import spark from "./../assets/images/logo/spark.png";
-
-import data from "./../data.json"
+import { tools } from "../data"
 
 const Tools = () => {
 
-  console.log("Valeur de data : ",data)
 
   return (
     <div className="divToolsContainer">
         <h2 className="mg-bt-20 small">Tools</h2>
         <div className="dis-flex space">
-            {/* <img src={spark} alt="" /> */}
-            <span>Spark</span>
-            <span>Airflow</span>
-            <span>Pandas</span>
-            <span>Kafka</span>
-            <span>Elasticsearch</span>
-            <span>Logstash</span>
-            <span>Kibana</span>
-            <span>D3.js</span>
-            <span>Chart.js</span>
-            <span>MongoDB</span>
+            { tools.tools.map( (logo,i) => <img className='mg-20' key={i} src={logo.default} alt="hello" />) }
             <span>Neo4j</span>
             <span>PostgreSQL</span>
             <span>MySQL</span>
---
+++ src/data.js
@@ -0,0 +1,14 @@
+export const tools = {
+    "tools" : [
+        require("./assets/images/logo/spark.png"),
+        require("./assets/images/logo/airflow.png"),
+        require("./assets/images/logo/pandas.png"),
+        require("./assets/images/logo/kafka.png"),
+        require("./assets/images/logo/elasticsearch.png"),
+        require("./assets/images/logo/logstash.png"),
+        require("./assets/images/logo/kibana.jpg"),
+        require("./assets/images/logo/d3.png"),
+        require("./assets/images/logo/chartjs.png"),
+        require("./assets/images/logo/mongo.png")
+    ]
+}
