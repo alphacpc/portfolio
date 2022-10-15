4695f6931b44dba4b262d051d77833595214f80a
alphacpc <alphacpc@gmail.com>
2022-10-15 21:47:22 +0000

    Correct text about me

--- src/components/About.js
+++ src/components/About.js
@@ -15,16 +15,16 @@ const About = () => {
             <div>
               <span className='circle'></span>
               <p>
-                <span>Jeune passioné des nouvelles technologies...</span> <br></br>
-                J'ai commencé à programmer à partir de la serie <strong><a href="https://www.allocine.fr/series/ficheserie_gen_cserie=20225.html" target="_blank" rel="noreferrer">Startup</a></strong>. 
+                <span>Jeune passionné des nouvelles technologies...</span> <br></br>
+                J'ai commencé à programmer à partir de la série <strong><a href="https://www.allocine.fr/series/ficheserie_gen_cserie=20225.html" target="_blank" rel="noreferrer">Startup</a></strong>. 
                 C'est dans ce dernier que j'ai entendu pour la 
                 première fois le mot <a href="https://fr.wikipedia.org/wiki/JavaScript" target="_blank" rel="noreferrer"><em>"JAVASCRIPT"</em></a>, lors d'une discussion entre
                 l'actrice principale et le fils d'un de ses collaborateurs, et depuis 
                 je m'en sépare plus [ il est devenu mon quotidien en quelque sorte !!! ]. 
                 Il y a environ 5 ans, je ne connaissais rien de la 
-                programmation mais aujourd'hui je fais parti des <em>50 développeurs sénégalais </em>
-                basé au Sénégal les plus actifs sur <a href="https://commits.top/senegal.html" rel="noreferrer" target="_blank">Github</a>. 
-                Aujourd'hui je touche presque à tous dans le cycle de développement de logiciel.
+                programmation mais aujourd'hui je fais partie des <em>50 développeurs sénégalais </em>
+                basés au Sénégal les plus actifs sur <a href="https://commits.top/senegal.html" rel="noreferrer" target="_blank">Github</a>. 
+                Aujourd'hui je touche presque à tout dans le cycle de développement de logiciel.
               </p>
            
             </div>
