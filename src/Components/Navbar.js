6d28292173e14f5ac619ba17b3502c7fae5a798e
alphacpc <alphacpc@gmail.com>
2023-07-29 11:23:24 +0000

    Updated project by some bug with package material-ui-tailwind

---
+++ src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Navbar} from "@material-tailwind/react"
+
+const NavbarComponent = () => {
+  return (
+    <header>
+      <Navbar>Hell</Navbar>
+        
+    </header>
+  )
+}
+
+export default NavbarComponent
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,44 +1,16 @@
-import React from 'react'
-import CardService from '../components/CardService'
-import Navbar from '../components/Navbar'
+import React from 'react';
+import NavbarComponent from '../Components/Navbar';
 
-import image from "./../assets/images/photo3.png"
 
 
 const Home = () => {
 
 
-
-    const servicesList = ["DevOps", "Data enginer", "Blockchain", "Computer vision", "Automatisation"]
-
-
     return (
-        <div className="divContainer divHomePage">
-            <div className="divIntro">
-                <Navbar/>
-
-                <div className="divSectionHeader">
-                    <div className="divLeft">
-                        <h1>AMIGO AAAAAA AAAAA</h1>
-                        <h3>FullStack Developer</h3>
-                        <h2>Junior Data Engineer</h2>
-                        <p>Devops - Automatisation - Blockchain - Computer Vision</p>
-                    </div>
-                    <div className="divRight">
-                        <div className='one'></div>
-                        <div className='second'></div>
-                        <img src={image} alt="Profil user"/>
-                    </div>
-                </div>
-
-
-                <div className="divConatinerService">
-                    {
-                        servicesList.map( service =>  <CardService title={service} />)
-                    }
-                </div>
-            </div>
-        </div>
+        <main className="divContainer divHomePage">
+            <h1>Hello DAKAR Bug</h1>
+            <NavbarComponent/>
+        </main>
     )
 }
 
