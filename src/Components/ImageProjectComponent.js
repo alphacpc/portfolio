8e64e9e3076ae9af5511fb9aad0cf339d957e0ec
alphacpc <alphacpc@gmail.com>
2023-10-07 13:01:07 +0000

    Added router page

--- src/Components/ImageProjectComponent.js
+++ src/Components/ImageProjectComponent.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const ImageProjectComponent = ({url_image, alt_image, width}) => {
   return (
-    <div className={`${width} flex border-8 border-white bg-white rounded-lg`}>
-      <img src={url_image} alt={alt_image} className="object-fill rounded-lg"/>
+    <div className={`${width} flex border-8 border-slate-50`}>
+      <img src={url_image} alt={alt_image} className="object-fill"/>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import "./../src/Assets/Styles/app.css"
+import { RouterProvider} from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+import { createBrowserRouter} from "react-router-dom";
+
+
+export const router = createBrowserRouter([
+  { 
+    path: "/",
+    element: <App />,
+    errorElement : <p>Error</p>,
+  },
+]);
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+      <RouterProvider router={router} />
+  </React.StrictMode>)
