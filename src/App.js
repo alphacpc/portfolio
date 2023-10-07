6bffbf736db684a9ad2c6373ce29fa8a82e4246e
alphacpc <alphacpc@gmail.com>
2023-10-07 11:45:34 +0000

    Added image

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ function App() {
   return (
     <div className="App">
       <p className="bg-orange-300">Lorem</p>
+      <img src="https://drive.google.com/file/d/1q3V3TEiahUnUCDPj43ePqV_IE09fFaDx/view?usp=sharing" alt="hey"/>
     </div>
   );
 }
