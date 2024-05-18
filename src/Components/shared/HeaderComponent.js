1fad2e8524a8b86f19333655da4ae19cf3939c62
alphacpc <alphacpc@gmail.com>
2024-05-18 13:16:35 +0000

    update design page

--- src/Components/shared/HeaderComponent.js
+++ src/Components/shared/HeaderComponent.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import LinkSocialComponent from '../LinkSocialComponent'
 import { BsMedium } from 'react-icons/bs'
-import { FiGithub, FiLinkedin, FiTwitter, FiYoutube, FiMap, FiCalendar, FiArrowDown } from 'react-icons/fi'
+import { FiGithub, FiLinkedin, FiTwitter, FiYoutube, FiMap, FiCalendar, FiArrowDown, FiMail } from 'react-icons/fi'
 import url_image from "./../../Assets/Images/image.png"
 
 const HeaderComponent = () => {
@@ -17,26 +17,26 @@ const HeaderComponent = () => {
                         </div>
                 </div>
 
-                <div className="relative flex items-center justify-between px-[10%]">
-                        <div className="bg-amber-300 flex-1">
+                <div className="relative flex items-center justify-between px-[10%] py-[5%]">
+                        <div className="flex-1">
                                 <div>
                                         <span className="bg-orange-700 p-2 rounded-md shadow-lg mr-2">Fullstack Developer</span>
                                         <span className="bg-orange-700 p-2 rounded-md shadow-lg mr-2">Junior DevOps</span>
                                         <span className="bg-orange-700 p-2 rounded-md shadow-lg mr-2">Data Engineer</span>
                                         <span className="bg-orange-700 p-2 rounded-md shadow-lg">Fraud Analyst</span>
                                 </div>
-                                <h1 className="font-semibold text-6xl mt-4 text-white">Alpha amadou DIALLO</h1>
-                                <p className="">
+                                <h1 className="font-semibold text-6xl mt-10 text-white">Alpha amadou DIALLO</h1>
+                                <p className="my-10 text-xl text leading-8">
                                         Versatile developer with skills in web development, DevOps, 
                                         data engineering and fraud analysis. Passionate about technology, mathematics and agriculture.
                                 </p>
                                 <div>
-                                        <a href='#'>Download CV</a>
+                                        <a href='#' className="p-4 uppercase rounded-md bg-green-500">Download CV</a>
                                 </div>
                         </div>
-                        <div className="bg-green-200 relative flex justify-center flex-1">
-                                <div className="bg-red-300 p-[250px] -z-2 absolute top-10 right-0 rounded-full " />
+                        <div className="relative flex justify-center flex-1">
                                 <img src={url_image} alt="image" className=""/>
+                                <div className="bg-red-300 bg-po p-[250px] z-2 opacity-75 absolute top-10 right-0 rounded-full " />
                         </div>
                 </div>
 
@@ -44,6 +44,7 @@ const HeaderComponent = () => {
                 <div className="p-4 bg-amber-200 absolute bottom-0 w-full flex items-center justify-between">
                         <FiMap/> Senegal, Dakar
                         <FiCalendar/> 12/12/2024
+                        <FiMail /> alphacpc@gmail.com
                         <FiArrowDown className="bg-emerald-500 rounded-full p-2" size={50} color='white'/>
                 </div>
       </header>
